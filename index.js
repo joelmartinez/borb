2d54515616578d197d5b6b1aed9120f3e7f2fd0b
Joel Martinez <joelmartinez@gmail.com>
2023-06-26 16:38:05 -0400

    initial checking ... spinning box

---
+++ index.js
@@ -0,0 +1,32 @@
+import * as THREE from 'three';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+var material = new THREE.MeshBasicMaterial({
+    color: 0x0095DD,
+    wireframe: true,
+    wireframeLinewidth: 2
+});
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+const directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
+scene.add( directionalLight );
+
+camera.position.z = 5;
+
+function animate() {
+	requestAnimationFrame( animate );
+
+    cube.rotation.x += 0.01;
+cube.rotation.y += 0.01;
+
+	renderer.render( scene, camera );
+}
+animate();
